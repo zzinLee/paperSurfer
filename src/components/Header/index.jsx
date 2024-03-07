7669c37bc1cb0533152b3a3f27946944130eb430
zzinLee <sujin951017@gmail.com>
2024-03-07 17:52:46 +0900

    :sparkles: Feat: 상단 Header 컴포넌트 추가

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,15 @@
+function Header() {
+  return (
+    <nav className="flex items-center justify-between shadow-md">
+      <div>
+        <img
+          src="/assets/papersurferHome.png"
+          className="w-200"
+        ></img>
+      </div>
+      <div className="text-center px-30">안녕하세요 연구원님!</div>
+    </nav>
+  );
+}
+
+export default Header;
