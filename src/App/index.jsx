d40b592418793c8ce2d4316b75788b5fb83fa9cc
zzinLee <sujin951017@gmail.com>
2024-03-08 20:51:54 +0900

    :lipstick: Design: App 에 Paper side bar 추가

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,12 +1,19 @@
 import Header from "../components/Header";
 import CollectionSidebar from "../components/CollectionSidebar";
+import PaperSidebar from "../components/PaperSidebar";
+
+import { useRootCollectionStore } from "../stores/collection";
 
 function App() {
+  const { collection } = useRootCollectionStore();
+  const isRootSelected = collection.key !== 0 && collection.collectionName !== "";
+
   return (
     <div className="relative flex flex-col h-screen">
       <Header />
-      <div className="relative z-0 h-full bg-slate-100">
+      <div className="relative z-0 flex flex-row h-full bg-slate-100">
         <CollectionSidebar />
+        {isRootSelected && <PaperSidebar />}
       </div>
     </div>
   );
