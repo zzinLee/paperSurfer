4db7eeead4e33eeb206091250bb3f18674fe1ba5
zzinLee <sujin951017@gmail.com>
2024-03-08 15:52:27 +0900

    💄 Design: App UI 변경, 헤더 z-index 포지션 추가

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,5 +1,12 @@
+import Header from "../components/Header";
+
 function App() {
-  return <div className="bg-sora">paper surfer boiler plate</div>;
+  return (
+    <div className="relative flex flex-col h-screen">
+      <Header />
+      <div className="relative z-0 h-full bg-slate-100"></div>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,13 @@
 function Header() {
   return (
-    <nav className="flex items-center justify-between shadow-md">
+    <nav className="relative z-10 flex items-center justify-between shadow-md">
       <div>
         <img
           src="/assets/papersurferHome.png"
           className="w-200"
         ></img>
       </div>
-      <div className="text-center px-30">안녕하세요 연구원님!</div>
+      <div className="text-xl text-center px-30">안녕하세요 연구원님!</div>
     </nav>
   );
 }
