08b57d460436b5094d7ea12c7096ac3d0e906f37
zzinLee <sujin951017@gmail.com>
2024-03-09 13:23:10 +0900

    :memo: Docs: 유효성 평가 및 경고 문구 추가

--- src/components/NewCollectionInput/index.jsx
+++ src/components/NewCollectionInput/index.jsx
@@ -3,15 +3,15 @@ import { CiCircleRemove } from "react-icons/ci";
 
 import { useCollectionStore } from "../../stores/collection";
 
-function NewCollectionInput({ clickButton }) {
+function NewCollectionInput({ toggle }) {
   const { setCollectionList } = useCollectionStore();
+  const [showAlert, setShowAlert] = useState(false);
   const [collectionName, setCollectionName] = useState("");
 
   function submitCollection (ev) {
     ev.preventDefault();
-    clickButton();
 
-    if (!collectionName.length) {
+    if (!collectionName.length || collectionName.length > 28) {
       return;
     }
 
@@ -21,24 +21,32 @@ function NewCollectionInput({ clickButton }) {
     };
 
     setCollectionList(newCollection);
+    toggle();
   }
 
   function typeInput(ev) {
-    setCollectionName(ev.target.value);
+    if (showAlert) {
+      setShowAlert(false);
+    }
+
+    if (ev.target.value.length > 28 || !ev.target.value.length) {
+      setShowAlert(true);
+    }
+
+    const userInput = ev.target.value;
+
+    setCollectionName(userInput.trim());
   }
 
   return (
     <div className="w-11/12 p-10 m-10 bg-white shadow-m min-w-100 rounded-xl h-fit">
       <div className="flex flex-row gap-10">
-        <button onClick={clickButton}>
+        <button onClick={toggle}>
           <CiCircleRemove className="text-purple size-30" />
         </button>
-        <p className="m-4 font-sans">새로운 컬렉션</p>
+        <p className="m-4 font-nanumNeo">새로운 컬렉션</p>
       </div>
-      <form
-        className="flex flex-col gap-10 p-5"
-        onSubmit={submitCollection}
-      >
+      <form className="flex flex-col gap-10 p-5" onSubmit={submitCollection}>
         <input
           type="text"
           className="p-5 rounded-sm bg-backgroundCollection"
@@ -46,7 +54,8 @@ function NewCollectionInput({ clickButton }) {
           autoFocus
           placeholder="컬렉션 이름을 입력하세요..."
         />
-        <button type="submit" className="self-end p-5 text-white rounded-md w-60 bg-purple">
+        {showAlert && <div className="text-pink-600 text-15 font-nanumNeo">1자 이상 28자 이내로 작성</div>}
+        <button type="submit" className="self-end p-5 text-white rounded-md font-nanumNeo w-60 bg-purple">
           확인
         </button>
       </form>
