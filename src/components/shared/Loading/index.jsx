539adc6f2123e98b093758404022dec49f3e7fd3
zzinLee <sujin951017@gmail.com>
2024-03-10 00:23:29 +0900

    :sparkles: Feat: Loading 컴포넌트 추가

---
+++ src/components/shared/Loading/index.jsx
@@ -0,0 +1,13 @@
+function Loading() {
+  return (
+    <div>
+      <img
+        src="assets/loading.gif"
+        alt="Loading"
+        width="50"
+      />
+    </div>
+  );
+}
+
+export default Loading;
