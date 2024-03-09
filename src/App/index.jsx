3f8d1cc4da880ce44965639fb16e5e294ce675b6
zzinLee <sujin951017@gmail.com>
2024-03-10 00:21:53 +0900

    :sparkles: Feat: 리액트 라우터 추가

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,13 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+
 import Header from "../components/Header";
+import SearchPage from "../pages/SearchPage";
 import Home from "../pages/Home";
-
-
 
 function App() {
   return (
     <div className="relative flex flex-col h-screen">
       <Header />
-      <Home />
+      <Routes>
+        <Route path="/" exact element={<Home />}>
+          <Route path="/search" element={<SearchPage />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App";
 
 import "./main.css";
 
 const rootElement = document.getElementById("root");
+const router = createBrowserRouter([
+  {
+    path: "*",
+    element: <App />
+  }
+]);
 
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
