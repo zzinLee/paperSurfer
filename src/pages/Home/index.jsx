7abae47b5cb52317d24a6e6b0d7f36a77504fe6a
zzinLee <sujin951017@gmail.com>
2024-03-12 07:38:55 +0900

    :memo: Docs: Home 구조 변경

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -8,16 +8,16 @@ import { usePaperListStore } from "../../stores/paper";
 
 function Home() {
   const { collection } = useRootCollectionStore();
-  const { paperList } = usePaperListStore();
+  const papers = usePaperListStore();
   const isRootCollectionSelected = collection.key !== 0 && collection.collectionName !== "";
-  const isCollectionPaperExist = paperList.length > 0;
+  const isPaperExist = papers.length > 0;
 
   return (
     <div className="relative z-0 flex flex-row h-full bg-bgColor">
       <CollectionSidebar />
       {isRootCollectionSelected && <PaperSidebar />}
-      {isRootCollectionSelected && !isCollectionPaperExist && <Navigate to={`/search/${collection.key}`} />}
-      {!isRootCollectionSelected && !isCollectionPaperExist && <Navigate to="/" />}
+      {isRootCollectionSelected &&  !isPaperExist && <Navigate to={`/search/${collection.key}`} />}
+      {!isRootCollectionSelected && !isPaperExist && <Navigate to="/" />}
       <Outlet />
     </div>
   );
