33f11552f663aaa8bcc463acf0c6c415ce12f3b4
zzinLee <sujin951017@gmail.com>
2024-03-12 07:47:15 +0900

    :memo: Docs: Params 사용 위해 라우트 path 수정

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -10,7 +10,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" exact element={<Home />}>
-          <Route path="/search/:collection" element={<SearchPage />} />
+          <Route path="/search/:collectionId" element={<SearchPage />} />
         </Route>
       </Routes>
     </div>
