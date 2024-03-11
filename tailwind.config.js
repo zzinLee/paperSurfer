d22ebd7c72f44305e108220ec400ae498bf82679
zzinLee <sujin951017@gmail.com>
2024-03-11 15:45:19 +0900

    🔧 Docs: tailwind config 파일 수정 | theme colors 변경

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,21 +16,22 @@ export default {
       colors: {
         backgroundCollection: "#D9D9D9",
         backgroundList: "#D0D0F9",
+        bgColor: "#F1F2F3",
         sora: "#7273D9",
-        gray: "A7A7A7",
-        purple: "#A880D9",
+        customPurple: "#A880D9",
         purpleGray: "#D1C6DE",
-        green: "#00C75C",
-        yellow: "#FFCF24",
-        red: "FF2424",
-        transWhite: "rgba(255, 255, 255, 0.5)",
+        customGreen: "#00C75C",
+        customYellow: "#FFCF24",
+        customRed: "#FF2424",
+        alert: "#D72E7C",
+        transWhite: "rgba(255, 255, 255, 0.5)"
       },
       borderWidth: px0_10,
       fontSize: px0_100,
       lineHeight: px0_100,
       minWidth: px0_200,
       minHeight: px0_200,
-      spacing: px0_300,
+      spacing: px0_300
     }
   },
   plugins: []
