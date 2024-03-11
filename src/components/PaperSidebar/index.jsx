d313c18a8efcf0a823a39ee6b1ff11f093cdbb2f
zzinLee <sujin951017@gmail.com>
2024-03-12 07:53:03 +0900

    :memo: Docs: 추가한 paper list 보여주는 기능 추가

--- src/components/PaperSidebar/index.jsx
+++ src/components/PaperSidebar/index.jsx
@@ -1,12 +1,38 @@
+import { BsBook } from "react-icons/bs";
+
 import { useRootCollectionStore } from "../../stores/collection";
+import { usePaperListStore } from "../../stores/paper";
+
+const CLASS_PAPER_LIST = "m-8 p-8 truncate text-14 font-nanumNeo rounded-md shadow-md bg-white hover:bg-gray-200";
+const CLASS_PAPER_SIDEBAR = "flex flex-col items-center h-full shadow-md bg-backgroundList w-220 font-nanumNeo";
+const CLASS_NO_PAPER_LIST = "p-8 text-white text-14 font-nanumNeo";
+const CLASS_COLLECTION_NAME = "p-8 m-10 text-white break-words rounded-md shadow-md min-w-130 bg-violet-500";
 
 function PaperSidebar() {
   const { collection } = useRootCollectionStore();
+  const papers = usePaperListStore();
+  const paperList = papers[collection.key];
+  const paperListElements =
+    paperList &&
+    paperList.map((paper) => (
+      <li
+        className={CLASS_PAPER_LIST}
+        key={paper.doi}
+      >
+        {paper.title}
+      </li>
+    ));
 
   return (
-    <section className="flex flex-col items-center h-full shadow-md bg-backgroundList min-w-200">
-      <div className="w-full p-5 mb-5 text-white bg-sora">
-        <h1 className="px-5 break-words min-w-130 text-balance">{collection.collectionName}</h1>
+    <section className={CLASS_PAPER_SIDEBAR}>
+      <div className="w-full text-center">
+        <div className={CLASS_COLLECTION_NAME}>
+          <BsBook className="inline mr-4" />
+          {collection.collectionName}
+        </div>
+        <ul>
+          {paperList ? paperListElements : <li className={CLASS_NO_PAPER_LIST}>등록된 논문이 없습니다.</li>}
+        </ul>
       </div>
     </section>
   );
