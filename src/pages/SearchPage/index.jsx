8fd08035ae9e22de7d2fee281975d827a342bd8c
zzinLee <sujin951017@gmail.com>
2024-03-12 17:52:12 +0900

    ✨ Feat: 플로팅버튼추가 네비게이터 이동

--- src/pages/SearchPage/index.jsx
+++ src/pages/SearchPage/index.jsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { GoPencil } from "react-icons/go";
 
 import PaperSearchBar from "../../components/PaperSearchBar";
 import Loading from "../../components/shared/Loading";
 import PaperSearchList from "../../components/PaperSearchList";
 import PaperSidebar from "../../components/PaperSidebar";
 
+const CLASS_FLOATING_BUTTON = "bg-violet-500 absolute p-8 rounded-full shadow-xl top-30 right-30 text-32 text-white";
+
 function SearchPage() {
+  const navigator = useNavigate();
+  const { collectionId } = useParams();
   const [isLoading, setIsLoading] = useState(false);
   const [searchList, setSearchList] = useState([]);
   const isSearchListExist = searchList.length !== 0;
@@ -18,6 +24,9 @@ function SearchPage() {
         {isLoading && <Loading />}
         {isSearchListExist && <PaperSearchList searchList={searchList} />}
       </section>
+      <button className={CLASS_FLOATING_BUTTON}>
+        <GoPencil onClick={() => navigator(`/${collectionId}`)} />
+      </button>
     </>
   );
 }
