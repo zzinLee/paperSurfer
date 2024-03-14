a39415299935f6b9945dcbc01bca43b463e9a973
zzinLee <sujin951017@gmail.com>
2024-03-14 10:39:32 +0900

    ✨Feat: utils 에 기능 함수 추가 response 포맷팅 함수 유틸로 추가 chart data 포맷팅 함수 유틸로 추가

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -4,4 +4,42 @@ function decodedString(string) {
   return _.unescape(string).replace(/<\/?[^>]+(>|$)/g, "");
 }
 
-export { decodedString };
+function formattingResponse(response) {
+  const rawAuthorList = response.author;
+  const authorList =
+    rawAuthorList &&
+    rawAuthorList
+      .filter((author) => author.family && author.given)
+      .map((author) => `${author.family} ${author.given}`);
+
+  return {
+    doi: response.DOI,
+    url: response.URL,
+    title: decodedString(response?.title[0]) || "제목 정보 없음",
+    references: response["reference-count"],
+    citations: response["is-referenced-by-count"],
+    createdAt: response?.created["date-parts"],
+    containerTitle: decodedString(response["container-title"][0]),
+    authors: decodedString(authorList?.join(", ")) || "저자 정보 없음",
+    refs: response?.reference?.map((value) => value.DOI).filter((val) => val)
+  };
+}
+
+function formattingChartData(paperList, collectionName) {
+  return {
+    name: collectionName,
+    children: paperList?.map(
+      (paper) => ({
+        doi: paper.doi,
+        url: paper.url,
+        title: paper.title,
+        citations: paper.citations,
+        createdAt: paper.createdAt,
+        authors: paper.authors,
+        children: paper.refs,
+      })
+    ) || [],
+  };
+}
+
+export { decodedString, formattingResponse, formattingChartData };
