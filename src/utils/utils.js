54bd6b309c5954442933f9acebbf164ec1763d2a
zzinLee <sujin951017@gmail.com>
2024-03-15 14:28:25 +0900

    ✨ Feat: 자손 노드 제목 정보 fetch 함수 추가

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,4 +1,7 @@
+import axios from "axios";
 import _ from "lodash";
+
+import API from "../utils/configAPI";
 
 function decodedString(string) {
   return _.unescape(string).replace(/<\/?[^>]+(>|$)/g, "");
@@ -11,15 +14,16 @@ function formattingResponse(response) {
     rawAuthorList
       .filter((author) => author.family && author.given)
       .map((author) => `${author.family} ${author.given}`);
+  const publishedAt = response.created?.["date-parts"]?.join(".");
 
   return {
     doi: response.DOI,
     url: response.URL,
-    title: decodedString(response?.title[0]) || "제목 정보 없음",
+    title: decodedString(response.title?.[0]) || "제목 정보 없음",
     references: response["reference-count"],
+    createdAt: publishedAt || "날짜 정보 없음",
     citations: response["is-referenced-by-count"],
-    createdAt: response?.created["date-parts"],
-    containerTitle: decodedString(response["container-title"][0]),
+    containerTitle: decodedString(response["container-title"]?.[0]) || "저널 정보 없음",
     authors: decodedString(authorList?.join(", ")) || "저자 정보 없음",
     refs: response?.reference?.map((value) => value.DOI).filter((val) => val)
   };
@@ -42,9 +46,45 @@ function formattingChartData(paperList, collectionName) {
   };
 }
 
+async function fetchChildrenNodes(root, nodeList, setChartData) {
+  const childrenList = nodeList.map((node) => ({
+    doi: node.doi,
+    title: node.title,
+    children: node.children
+  }));
+
+  const reqUrlEmbeddedChildrenList = childrenList.map((node) => ({
+    ...node,
+    reqURL: `${API.CROSSREF_WORKS_URL}?filter=doi:${node.children.join(",doi:")}&select=DOI,title,is-referenced-by-count`
+  }));
+
+  const childrenPromiseList =
+    reqUrlEmbeddedChildrenList.map((node) => axios.get(node.reqURL));
+  const allResponse = await Promise.all(childrenPromiseList);
+  const childrenDataList = allResponse.map((res) => {
+    const items = res?.data?.message?.items;
+    const formattedItems = items.map((item) => ({
+      title: item?.title?.[0] || "제목 정보 없음",
+      doi: item.DOI,
+      citations: item["is-referenced-by-count"],
+      children: [],
+    }));
+
+    return formattedItems;
+  });
+
+  setChartData ({
+    ...root,
+    children: childrenList.map((sub, index) => ({
+      ...sub,
+      children: childrenDataList[index],
+    })),
+  });
+}
 
 export {
   decodedString,
   formattingResponse,
   formattingChartData,
+  fetchChildrenNodes,
 };
