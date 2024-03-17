b48fafec84178592cd5cff43b1260acac59b4c82
zzinLee <sujin951017@gmail.com>
2024-03-17 22:47:29 +0900

    ✨ Feat: Loading 컴포넌트 추가 | 원형 로딩 컴포넌트"

---
+++ src/components/shared/LoadingCircle/index.jsx
@@ -0,0 +1,8 @@
+const CLASS_LOADING_CIRCLE =
+  "border-4 border-transparent rounded-full h-80 w-80 animate-spin border-sora border-t-customPurple";
+
+function LoadingCircle() {
+  return <div className={CLASS_LOADING_CIRCLE}></div>;
+}
+
+export default LoadingCircle;
