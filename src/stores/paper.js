27655c14af2b4a38790241de6de63ed62164e74a
zzinLee <sujin951017@gmail.com>
2024-03-19 03:33:35 +0900

    ♻️ Refactor: paper store immer 미들웨어 적용

--- src/stores/paper.js
+++ src/stores/paper.js
@@ -1,48 +1,35 @@
 import { create } from "zustand";
 import { persist, createJSONStorage, devtools } from "zustand/middleware";
+import { immer } from "zustand/middleware/immer";
 
 const paperListStore = persist(
-  (set) => ({
+  immer((set) => ({
     paperList: {},
-    addPaperList: (paper, key) =>
-      set((prev) => {
-        if (prev.paperList[key]) {
-          return {
-            ...prev,
-            paperList: {
-              ...prev.paperList,
-              [key]: [...prev.paperList[key], paper]
-            }
-          };
-        } else {
-          return {
-            ...prev,
-            paperList: {
-              ...prev.paperList,
-              [key]: [paper]
-            }
-          };
-        }
-      }),
-    deletePaperList: (key) =>
-      set((prev) => {
-        delete prev.paperList[key];
-
-        return prev;
-      }),
-    deleteEachPaper: (key, doi) =>
-      set((prev) => {
-        const deletedArray = [...prev.paperList[key]].filter((paper) => paper.doi !== doi);
-
-        return {
-          ...prev,
-          paperList: {
-            ...prev.paperList,
-            [key]: deletedArray
-          }
+    addPaper: (key, paper) => set((state) => {
+      if (state.paperList[key]) {
+        state.paperList = {
+          ...state.paperList,
+          [key]: [...state.paperList[key], paper],
         };
-      })
-  }),
+      } else {
+        state.paperList = {
+          ...state.paperList,
+          [key]: [paper],
+        };
+      }
+    }),
+    deleteAllPaperList: (key) => set((state) => {
+      delete state.paperList[key];
+    }),
+    deletePaperFromStore: (key, doi) => set((state) => {
+      const deletedArray = state.paperList[key].filter((paper) => paper.doi !== doi);
+
+      state.paperList[key] = deletedArray;
+    }),
+    initPaperList: (key, starPaperList) => set((state) => {
+      state.paperList[key] = starPaperList;
+    }),
+  })),
   {
     name: "paper-storage",
     storage: createJSONStorage(() => sessionStorage)
