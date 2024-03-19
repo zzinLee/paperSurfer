947cb7ae7e4d67f077f15bcb6e221b1a1539155f
zzinLee <sujin951017@gmail.com>
2024-03-20 01:00:18 +0900

    ✨ Feat: Print Page 테이블 페이지 추가

---
+++ src/components/TableRow/index.jsx
@@ -0,0 +1,16 @@
+function TableRow({ paper }) {
+  return (
+    <tr className="bg-white border-b">
+      <th scope="row" className="w-1/4 p-8 font-extrabold text-gray-900">
+        {paper.title}
+      </th>
+      <td className="w-2/12 px-2 py-8">{paper.authors}</td>
+      <td className="w-1/12 px-2 py-8">{paper.createdAt}</td>
+      <td className="w-2/12 px-2 py-8">{paper.containerTitle}</td>
+      <td className="px-2 py-8">{paper.citations}</td>
+      <td className="w-1/12 py-8 pr-8">{paper.doi}</td>
+    </tr>
+  );
+}
+
+export default TableRow;
---
+++ src/pages/PrintPage/index.jsx
@@ -0,0 +1,60 @@
+import { useParams } from "react-router-dom";
+
+import TableRow from "../../components/TableRow";
+import ErrorPage from "../ErrorPage";
+
+import { usePaperStore } from "../../stores/paper";
+import { useCollectionStore } from "../../stores/collection";
+
+function PrintPage() {
+  const { collection } = useCollectionStore();
+  const { paperCollection } = usePaperStore();
+  const { collectionId } = useParams();
+  const paperList = paperCollection[collectionId];
+  const isPrintDataExist = paperList?.length > 0;
+
+  if (!isPrintDataExist) {
+    const message = "출력할 수 있는 정보가 없습니다.";
+
+    return <ErrorPage message={message} />;
+  }
+
+  const tableRowElemList = paperList.map((paper) => <TableRow key={paper.doi} paper={paper} />);
+
+  return (
+    <div className="content-center my-auto">
+      <div className="relative mx-10 overflow-x-auto font-nanumNeo">
+        <h1 className="py-10 text-center text-white bg-black">{collection[collectionId]}</h1>
+        <table className="w-full text-sm text-left text-gray-500 rtl:text-right">
+          <thead className="p-10 text-gray-700 uppercase text-md bg-gray-50">
+            <tr className="p-8">
+              <th scope="col" className="w-1/4 p-8 font-extrabold text-gray-900">
+                제목
+              </th>
+              <th scope="col" className="w-2/12 px-2 py-8">
+                저자
+              </th>
+              <th scope="col" className="w-1/12 px-2 py-8">
+                출판일자
+              </th>
+              <th scope="col" className="w-2/12 px-2 py-8">
+                저널명
+              </th>
+              <th scope="col" className="w-1/12 px-8 py-8">
+                인용횟수
+              </th>
+              <th scope="col" className="w-1/12 py-8 pr-8">
+                DOI
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {tableRowElemList}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default PrintPage;
