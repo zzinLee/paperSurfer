b2102ad64f35bccefa0b089fb9f9b70978ba38b7
zzinLee <sujin951017@gmail.com>
2024-03-20 17:23:40 +0900

    📝 Docs: paper 리스트내 버튼 클릭 시 논문 상세 모달 팝업

--- src/components/Paper/index.jsx
+++ src/components/Paper/index.jsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 import { FcEmptyTrash } from "react-icons/fc";
+
+import Modal from "../shared/Modal";
+import PaperCard from "../PaperCard";
 
 import { usePaperStore } from "../../stores/paper";
 import { useChartStore } from "../../stores/chart";
 
 function Paper({ paper }) {
+  const [isPaperCardOpen, setIsPaperCardOpen] = useState(false);
   const { collectionId } = useParams();
   const { deletePaperFromCollection } = usePaperStore();
   const { deletePaperFromChart, deletePaperFromStarCollection } = useChartStore();
@@ -18,8 +23,18 @@ function Paper({ paper }) {
     deletePaperFromStarCollection(collectionId, paper.doi);
   }
 
+  function clickPaper(ev) {
+    ev.preventDefault();
+
+    setIsPaperCardOpen(!isPaperCardOpen);
+  }
+
   return (
-    <div className="relative p-8 m-10 text-left bg-white rounded-sm shadow-md w-200">
+    <div
+      className="relative p-8 m-10 text-left bg-white rounded-sm shadow-md w-200 hover:bg-slate-50"
+      onClick={clickPaper}
+    >
+      {isPaperCardOpen && <Modal><PaperCard paper={paper} /></Modal>}
       <div className="font-extrabold truncate text-14">{paper?.title || "제목 정보 없음"}</div>
       <div className="truncate text-10 text-stone-400">{paper?.authors || "저자 정보 없음"}</div>
       <div className="inline-flex justify-between w-full">
