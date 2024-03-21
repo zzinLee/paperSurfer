e66e7d702ef455f6c1cb671870af365a6660de13
zzinLee <sujin951017@gmail.com>
2024-03-21 18:33:46 +0900

    🔧 Docs: vite.config.js 변경 | react-pdf

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,25 @@
-import { defineConfig } from "vite";
+import path from "node:path";
+import { createRequire } from "node:module";
+
+import { defineConfig, normalizePath } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 import react from "@vitejs/plugin-react-swc";
+
+const require = createRequire(import.meta.url);
+const cMapsDir = normalizePath(
+  path.join(path.dirname(require.resolve("pdfjs-dist/package.json")), "cmaps")
+);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+    targets:[{
+      src: cMapsDir,
+      dest: "",
+    }],
+  })],
   test: {
     globals: true,
     environment: "jsdom",
