5c99714cdec852d0609e2c46ceef5bc390c59fb3
zzinLee <sujin951017@gmail.com>
2024-03-25 15:11:37 +0900

    🔧 Docs: setupTests.js 추가 및 vite.config.js 경로수정

--- vite.config.js
+++ vite.config.js
@@ -30,7 +30,7 @@ export default defineConfig({
     },
     globals: true,
     environment: "jsdom",
-    setupFiles: "./src/setupTests.js",
+    setupFiles: "./setupTests.js",
     testMatch: ["./src/spec/*.spec.jsx"]
   }
 });
