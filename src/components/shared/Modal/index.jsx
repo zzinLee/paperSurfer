038efa18d0b3a33f765ad0c708bad9a8650d27c1
zzinLee <sujin951017@gmail.com>
2024-03-27 02:33:24 +0900

    🚑️ Hotfix:  Loading component 와 함께 쓰이는 Modal 컴포넌트 수정

--- src/components/shared/Modal/index.jsx
+++ src/components/shared/Modal/index.jsx
@@ -17,7 +17,7 @@ function Modal({ children, setModal }) {
         ref={outside}
         onClick={clickOutside}
       ></div>
-      <div className="fixed flex justify-center overflow-y-auto transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+      <div className="fixed flex justify-center transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
         {children}
       </div>
     </div>
