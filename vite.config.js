f81a61ef1241a6d2da3eb8896e73270f480a757a
zzinLee <sujin951017@gmail.com>
2024-04-01 15:32:48 +0900

    🔧 Docs: config 파일 수정

--- vite.config.js
+++ vite.config.js
@@ -27,6 +27,7 @@ export default defineConfig({
     coverage: {
       provider: "v8",
       reporter: ["text", "json", "html"],
+      reportsDirectory: "./src/spec/coverage",
     },
     globals: true,
     environment: "jsdom",
