9b74732ec99456136d1dd5f2614e3a30e96d8235
zzinLee <sujin951017@gmail.com>
2024-04-08 02:26:54 +0900

    ♻️ Refactor: mediaQuery 적용을 위한 DOM 추가

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -10,20 +10,29 @@ import FileViewPage from "../pages/FileViewPage";
 
 function App() {
   return (
-    <div className="relative flex flex-col h-screen">
-      <Header />
-      <Routes>
-        <Route path="/" exact element={<Home />}>
-          <Route path="/:collectionId/search" element={<SearchPage />} />
-          <Route path="/:collectionId/view" element={<ViewPage />} />
-          <Route path="/:collectionId/table" element={<TablePage />} />
-          <Route path="/error" element={<ErrorPage />} />
-        </Route>
-      </Routes>
-      <Routes>
-        <Route path="/file" element={<FileViewPage />} />
-      </Routes>
-    </div>
+    <>
+      <div className="relative hidden w-screen h-screen mobile-alert">
+        <div className="relative p-10 text-center">
+          <p>현재 서비스는 모바일 환경에서는 지원하지 않습니다.</p>
+          <p>웹에서 접속해주세요.</p>
+          <p>곧 모바일에서도 뵈어요! 🙌</p>
+        </div>
+      </div>
+      <div className="relative flex flex-col h-screen desktop-content">
+        <Header />
+        <Routes>
+          <Route path="/" exact element={<Home />}>
+            <Route path="/:collectionId/search" element={<SearchPage />} />
+            <Route path="/:collectionId/view" element={<ViewPage />} />
+            <Route path="/:collectionId/table" element={<TablePage />} />
+            <Route path="/error" element={<ErrorPage />} />
+          </Route>
+        </Routes>
+        <Routes>
+          <Route path="/file" element={<FileViewPage />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
