c6263cb5c0fb16bc04221cbeea4c33c19c3ed699
zzinLee <sujin951017@gmail.com>
2024-05-31 09:57:48 +0900

    ♻️ Refactor: PaperNodeCard 컴포넌트 .tsx 로 변경

--- src/components/PaperNodeCard/index.tsx
+++ src/components/PaperNodeCard/index.tsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useState, useEffect, type MouseEvent, type Dispatch, type SetStateAction } from "react";
 import axios from "axios";
 
 import LoadingCircle from "../shared/LoadingCircle";
@@ -8,6 +8,7 @@ import useChartStore from "../../stores/chart";
 import API from "../../utils/configAPI";
 import { STATUS, MAILTO } from "../../utils/constants";
 import { formattingResponse, decodedString } from "../../utils/utils";
+import type { ChartStoreState, ReferenceConfig, RootConfig, SearchResponseConfig, PaperConfig } from "../../types/interface";
 
 const CLASS_CARD_PROP = "flex flex-row gap-3 px-10 py-5";
 const CLASS_CARD_BUTTON = "px-8 py-2 text-white rounded-lg shadow-md hover:cursor-pointer text-16";
@@ -15,19 +16,25 @@ const CLASS_BADGE_PROP = "text-xs font-medium inline-flex items-center px-2.5 py
 const CLASS_BADGE =
   "text-sm font-semibold me-2 mr-4 px-2.5 py-0.5 rounded inline-flex items-center justify-center min-w-72";
 
-function PaperNodeCard({ nodeData, setModalOpen, setIsLoadingChild }) {
-  const { collectionId } = useParams();
-  const { changeNodeStatus, addChildrenToNode, addStarPaper } = useChartStore();
+interface PaperNodeCardProps {
+  nodeData: RootConfig;
+  setModalOpen: Dispatch<SetStateAction<boolean>>;
+  setIsLoadingChild: Dispatch<SetStateAction<boolean>>;
+}
 
-  const [paper, setPaper] = useState(null);
+function PaperNodeCard({ nodeData, setModalOpen, setIsLoadingChild }: PaperNodeCardProps) {
+  const { collectionId } = useParams() as { collectionId: string };
+  const { changeNodeStatus, addChildrenToNode, addStarPaper } = useChartStore() as ChartStoreState;
+
+  const [paper, setPaper] = useState<null | PaperConfig>(null);
   const [isLinkClick, setIsLinkClick] = useState(false);
 
   const isPaperExist = Boolean(paper);
-  const abstract = decodedString(paper?.abstract).replaceAll("Abstract", "").slice(0, 290);
+  const abstract = paper && paper.abstract && decodedString(paper.abstract).replaceAll("Abstract", "").slice(0, 290);
   const formattedAbstract = abstract ? abstract + "..." : "초록 정보 없음";
   const isStar = nodeData.status === STATUS.STAR;
 
-  const fetchPaperInformation = async (doi, setPaper) => {
+  const fetchPaperInformation = async (doi: string, setPaper: Dispatch<SetStateAction<null | PaperConfig>>) => {
     const res = await axios.get(`${API.CROSSREF_WORKS_URL}/${doi}`);
     const response = res?.data?.message;
     const paper = formattingResponse(response);
@@ -35,14 +42,14 @@ function PaperNodeCard({ nodeData, setModalOpen, setIsLoadingChild }) {
     setPaper(paper);
   };
 
-  const getChildrenNode = async (collectionId, parentNode) => {
+  const getChildrenNode = async (collectionId: string, parentNode: RootConfig) => {
     setIsLoadingChild(true);
 
     const res = await axios.get(
       `${API.CROSSREF_WORKS_URL}?filter=doi:${parentNode.doi}&select=reference&mailto=${MAILTO}`
     );
     const response = res?.data?.message;
-    const paperRefList = response?.items?.[0]?.reference;
+    const paperRefList: ReferenceConfig[] = response?.items?.[0]?.reference;
     const isRefExist = paperRefList && paperRefList.length > 0;
 
     if (!isRefExist) {
@@ -74,7 +81,7 @@ function PaperNodeCard({ nodeData, setModalOpen, setIsLoadingChild }) {
       return;
     }
 
-    const formattedChildrenList = childrenList?.map((child) => {
+    const formattedChildrenList = childrenList?.map((child: SearchResponseConfig): RootConfig => {
       const rawAuthorList = child.author;
       const authorList =
         rawAuthorList &&
@@ -89,6 +96,7 @@ function PaperNodeCard({ nodeData, setModalOpen, setIsLoadingChild }) {
         doi: child?.DOI,
         createdAt: child?.created?.["date-parts"]?.[0]?.join(".") || "출판일 정보 없음",
         author: authorList?.join(", ") || "저자 정보 없음",
+        children: [],
       };
     });
 
@@ -101,7 +109,7 @@ function PaperNodeCard({ nodeData, setModalOpen, setIsLoadingChild }) {
   }, [nodeData.doi]);
 
   useEffect(() => {
-    if (isLinkClick) {
+    if (isLinkClick && paper) {
       window.open(paper.url, "_blank", "noopener, noreferrer");
 
       setIsLinkClick(false);
@@ -111,10 +119,13 @@ function PaperNodeCard({ nodeData, setModalOpen, setIsLoadingChild }) {
 
   const clickStar = () => {
     const isChildrenExist = nodeData?.children && nodeData.children.length > 0;
+
+    if (!paper) return;
+
     const isReferenceExist = paper.references > 0;
 
     if (!isChildrenExist && isReferenceExist) {
-      getChildrenNode(collectionId, nodeData, addChildrenToNode);
+      getChildrenNode(collectionId, nodeData);
     }
 
     changeNodeStatus(collectionId, nodeData, STATUS.STAR);
@@ -138,7 +149,7 @@ function PaperNodeCard({ nodeData, setModalOpen, setIsLoadingChild }) {
     setModalOpen(false);
   };
 
-  const clickPaperLink = (ev) => {
+  const clickPaperLink = (ev: MouseEvent<HTMLAnchorElement>) => {
     ev.preventDefault();
 
     setIsLinkClick(true);
@@ -168,11 +179,11 @@ function PaperNodeCard({ nodeData, setModalOpen, setIsLoadingChild }) {
           </div>
           <div className="px-12 py-8">
             <h1 className="px-12 py-4 m-2 font-semibold tracking-tight">
-              <p className="p-4">{decodedString(paper?.title) || "제목 정보 없음"}</p>
+              <p className="p-4">{paper && decodedString(paper?.title) || "제목 정보 없음"}</p>
             </h1>
             <div className={CLASS_CARD_PROP}>
               <p className={`${CLASS_BADGE} bg-slate-100 text-slate-600`}>저널명</p>
-              <p className="text-sm text-slate-600">{decodedString(paper?.containerTitle) || "저널 정보 없음"}</p>
+              <p className="text-sm text-slate-600">{paper && decodedString(paper?.containerTitle) || "저널 정보 없음"}</p>
             </div>
             <div className={CLASS_CARD_PROP}>
               <p className={`${CLASS_BADGE} bg-blue-100 text-blue-800`}>출판일</p>
@@ -181,7 +192,7 @@ function PaperNodeCard({ nodeData, setModalOpen, setIsLoadingChild }) {
             <div className="inline-flex">
               <div className={CLASS_CARD_PROP}>
                 <p className={`${CLASS_BADGE} bg-indigo-100 text-indigo-800`}>저자</p>
-                <p className="text-sm text-slate-600 max-w-680">{decodedString(paper?.authors) || "저자 정보 없음"}</p>
+                <p className="text-sm text-slate-600 max-w-680">{paper && decodedString(paper?.author) || "저자 정보 없음"}</p>
               </div>
             </div>
             <div className={CLASS_CARD_PROP}>
