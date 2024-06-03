68876a5635694f7bd06d38a3ad714d6bc71254ab
zzinLee <sujin951017@gmail.com>
2024-06-04 00:12:21 +0900

    💄 Design: PaperSidebar 디자인 수정

--- src/components/PaperSidebar/index.tsx
+++ src/components/PaperSidebar/index.tsx
@@ -23,12 +23,18 @@ function PaperSidebar() {
   const isPaperListExist = paperListElements?.length > 0;
 
   return (
-    <aside className="flex flex-col items-center h-full overflow-auto shadow-sm min-w-220 font-nanumNeo bg-stone-50">
+    <aside className="flex flex-col items-center h-full overflow-auto shadow-sm max-w-220 min-w-220 font-nanumNeo bg-stone-50 text-15">
       <div className="w-full text-center break-words">
-        <div className="p-8 m-10 text-white rounded-sm min-w-130 bg-violet-700">
+        <div className="p-10 border border-r-0 border-l-0 border-t-0 border-b-2 border-stone-200">
           {currentCollectionName || "제목 내용 없음"}
         </div>
-        <ul>{isPaperListExist ? paperListElements : <li className="p-8 text-black-100 text-14">등록된 논문이 없습니다.</li>}</ul>
+        <ul>
+          {isPaperListExist ? (
+            paperListElements
+          ) : (
+            <li className="p-8 text-black-100 text-14 text-slate-700">등록된 논문이 없습니다.</li>
+          )}
+        </ul>
       </div>
       {isPaperListExist && (
         <Link to={`/${collectionId}/table`}>
