207308617e0b8a5d93744e8c7002bb80d539f1ee
zzinLee <sujin951017@gmail.com>
2024-06-03 23:55:52 +0900

    ✨ Feat: WelcomePage 추가

---
+++ src/pages/WelcomePage/index.tsx
@@ -0,0 +1,12 @@
+
+
+function WelcomePage() {
+  return (
+    <main className="relative h-full w-full">
+      <img src="/assets/description.png" alt="" className="absolute z-10 top-10 right-10 w-1/3" />
+      <div className="w-full bg-cover opacity-40 h-full bg-gradient-to-r to-cyan-200 from-violet-200"></div>
+    </main>
+  );
+}
+
+export default WelcomePage;
