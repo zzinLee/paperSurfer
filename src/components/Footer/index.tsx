a3a8fad7267b7adeaacb72d53b3b95dd0d1b10cd
zzinLee <sujin951017@gmail.com>
2024-06-14 01:46:54 +0900

    💄 Design: Footer 미디어쿼리 수정

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,15 +1,25 @@
 function Footer() {
   return (
     <footer className="bg-slate-50">
-      <div className="w-full max-w-screen-xl mx-auto md:py-8">
+      <div className="w-full max-w-screen-xl mx-auto py-8 px-12 sm:px-4">
         <div className="sm:flex sm:items-center sm:justify-between">
-          <a href="https://papersurfer.zzinlee.dev" className="flex items-center mb-4 sm:mb-0 space-x-3">
-            <img src="/assets/papersurferLogo.png" className="w-60" alt="PaperSurfer 로고" />
-            <span className="self-center text-xl whitespace-nowrap">PaperSurfer</span>
+          <a
+            href="https://papersurfer.zzinlee.dev"
+            className="flex items-center mb-4 sm:mb-0 space-x-3"
+          >
+            <img
+              src="/assets/papersurferLogo.png"
+              className="w-[3rem] mb:w-[1.5rem]"
+              alt="PaperSurfer 로고"
+            />
+            <span
+              className="self-center text-xl whitespace-nowrap sm:text-[1.2rem]"
+            >PaperSurfer
+            </span>
           </a>
           </div>
         <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
-        <span className="block text-sm text-gray-500 sm:text-center">
+        <span className="block text-[1rem] text-gray-500 sm:text-center mb:text-[0.8rem]">
           Copyright © 2024{" "}
           <a href="https://papersurfer.zzinlee.dev" className="hover:underline">
             PaperSurfer
