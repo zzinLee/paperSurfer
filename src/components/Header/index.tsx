ba86febaa6df2ed73831873e894497aa72976033
zzinLee <sujin951017@gmail.com>
2024-06-14 01:46:40 +0900

    💄 Design: Header 미디어쿼리 수정

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -15,31 +15,31 @@ function Header() {
     setPageState("Home");
   };
 
-  const [pdfButtonBgColor, homeButtonBgColor] = pageState === "PDFViewer" ? ["bg-black", ""] : ["", "bg-black"];
+  const [pdfButtonBgColor, homeButtonBgColor] = pageState === "PDFViewer" ? ["bg-slate-400", ""] : ["", "bg-slate-400"];
   const [pdfButtonTextColor, homeButtonTextColor] = pageState === "PDFViewer" ? ["text-white", ""] : ["", "text-white"];
 
   return (
-    <nav className="flex flex-row justify-between border-b-gray-200 border">
+    <nav className="flex flex-row justify-between items-center border-b-gray-200 border sm:p-4">
       <Link to="/">
-        <img src="/assets/papersurferHome.png" className="h-68" />
+        <img src="/assets/papersurferHome.png" className="ease-in-out h-[3.5rem] sm:h-[2.8rem] duration-100" />
       </Link>
-      <ul className="flex items-center mr-52 gap-12">
+      <ul className="flex items-center mr-[3rem] gap-[0.6rem] sm:text-[0.8rem] mb:mr-[0.5rem]">
         <Link to="/file">
           <li
-            className={`inline-flex items-center gap-4 py-8 px-12 rounded-md text-center cursor-default ${pdfButtonBgColor} ${pdfButtonTextColor}`}
+            className={`flex justify-center items-center gap-4 py-6 px-8 rounded-md cursor-default ${pdfButtonBgColor} ${pdfButtonTextColor} sm:py-4`}
             onClick={handleViewerClick}
           >
             <FaFilePdf size="18" />
-            PDF VIEWER
+            <span className="mb:hidden">PDF VIEWER</span>
           </li>
         </Link>
         <Link to="/">
           <li
-            className={`inline-flex items-center gap-4 py-8 px-12 rounded-md text-center cursor-default ${homeButtonBgColor} ${homeButtonTextColor}`}
+            className={`flex justify-center items-center gap-4 py-6 px-8 rounded-lg text-center cursor-default ${homeButtonBgColor} ${homeButtonTextColor} sm:py-4`}
             onClick={handleHomeClick}
           >
             <IoHomeSharp size="18" />
-            HOME
+            <span className="mb:hidden">HOME</span>
           </li>
         </Link>
         <a href="https://github.com/zzinLee/paperSurfer">
