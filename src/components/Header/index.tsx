6b77fd3b5af86b5568a5934d3f2421b14fa748b7
zzinLee <sujin951017@gmail.com>
2024-06-17 11:34:26 +0900

    🐛 Fix: header 아이콘 클릭 시 우측 바 상태 미 변경 되는 버그"

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -21,7 +21,11 @@ function Header() {
   return (
     <nav className="flex flex-row justify-between items-center border-b-gray-200 border sm:p-4">
       <Link to="/">
-        <img src="/assets/papersurferHome.png" className="ease-in-out h-[3.5rem] sm:h-[2.8rem] duration-100" />
+        <img
+          src="/assets/papersurferHome.png"
+          className="ease-in-out h-[3.5rem] sm:h-[2.8rem] duration-100"
+          onClick={handleHomeClick}
+        />
       </Link>
       <ul className="flex items-center mr-[3rem] gap-[0.6rem] sm:text-[0.8rem] mb:mr-[0.5rem]">
         <Link to="/file">
